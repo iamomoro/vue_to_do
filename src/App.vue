1c8e0fefb0b345a531f1cc3713cb95641500daa1
Elvis Omoro <iamomoroit@gmail.com>
2022-10-30 20:30:10 +0300

    create page layout

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,70 @@ const todos_asc = computed(() => todos.value.sort((a, b) =>{
     return b.createdAt - a.createdAt
 
 }))
+
+const addTodo = () => {
+    if (input_content.value.trim() === '' || input_category.value === null) {
+
+    }
+    
+}
+
+watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+})
+
+onMounted(()=> {
+    name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
-  
+   <main class="app">
+        <section class="greeting">
+            <h2 class="title">
+                What's Up, <input type="text" placeholder="Name Here" v-model="name"/>
+            </h2>
+        </section>
+
+        <section class="create-todo">
+            <h3>CREATE A TODO</h3>
+
+            <form @submit.prevent="addTodo">
+                <h4>What's on your to-do list?</h4>
+                <input 
+                    type="text" 
+                    placeholder=" e.g. Learn Javascript"
+                    v-model="input_content"
+                />
+                <h4>Pick a Category</h4>
+                <div class="options">
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="category" 
+                            value="work"
+                            v-model="input_category"
+                        />
+                        <span class="bubble business"></span>
+                        <div>Work</div>
+                    </label>
+
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="category" 
+                            value="personal"
+                            v-model="input_category"
+                        />
+                        <span class="bubble personal"></span>
+                        <div>Personal</div>
+                    </label>
+
+                </div>
+
+               <input type="submit" value="Add todo">
+            </form>
+        </section>
+   </main>
 </template>
 
